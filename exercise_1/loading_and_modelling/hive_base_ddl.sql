6f218bb3763f40493059a215a2109bb4f6e828bc
James Peng <james.y.peng@gmail.com>
2016-06-04 20:45:15 +0800

    Add Timely_and_Effective_Care

--- exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS Hospital_Info;
-
 CREATE EXTERNAL TABLE Hospital_Info
 (Provider_ID string,
 Hospital_name string,
@@ -22,7 +21,6 @@ STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/Hospital_Info';
 
 DROP TABLE IF EXISTS Measure_Dates;
-
 CREATE EXTERNAL TABLE Measure_Dates
 (measure_name string,
 measure_id string,
@@ -38,3 +36,29 @@ WITH SERDEPROPERTIES (
 )
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/Measure_Dates';
+
+DROP TABLE IF EXISTS Timely_and_Effective_Care;
+CREATE EXTERNAL TABLE Timely_and_Effective_Care
+(Provider_ID string,
+Hospital_name string,
+Address string,
+City string,
+state string,
+ZipCode string,
+County string,
+PhoneNumber string,
+condition string,
+measure_id string,
+measure_name string,
+sample string,
+footnote string,
+start_date string,
+end_date string)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/Timely_and_Effective_Care';
