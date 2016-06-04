e4946c17fdac556132b751c8e952a7b875895267
James Peng <james.y.peng@gmail.com>
2016-06-04 20:52:21 +0800

    add Readmissions_and_Deaths

--- exercise_1/loading_and_modelling/hive_base_ddl.sql
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -62,3 +62,35 @@ WITH SERDEPROPERTIES (
 )
 STORED AS TEXTFILE
 LOCATION '/user/w205/hospital_compare/Timely_and_Effective_Care';
+
+
+
+DROP TABLE IF EXISTS Readmissions_and_Deaths;
+CREATE EXTERNAL TABLE Readmissions_and_Deaths
+(Provider_ID string,
+Hospital_name string,
+Address string,
+City string,
+state string,
+ZipCode string,
+County string,
+PhoneNumber string,
+measure_name string,
+measure_id string,
+compared_to_national string,
+denominator string,
+score string,
+lower_estimate string,
+higher_estimate string,
+footnote string,
+start_date string,
+end_date string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare/Readmissions_and_Deaths';
