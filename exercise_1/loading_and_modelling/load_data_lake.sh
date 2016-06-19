ba94f6fbe12045e6267f6a127efb21a5779693ad
James Peng <james.y.peng@gmail.com>
2016-06-19 13:27:50 +0800

    Update: Use latest flat files Add outcome file to HDFS

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -40,6 +40,9 @@ cd $ORIGINAL_FILE_PATH
 
 wget -O Hospital_Revised_Flatfiles.zip https://data.medicare.gov/views/bg9k-emty/files/7825b9e4-e595-4f25-86e0-32a68d7ac7a4?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
 
+# http://medicare.gov/download/HospitalCompare/2015/December/HOSArchive_Revised_FlatFiles_20151210.zip
+# https://downloads.cms.gov/medicare/Hospital_Revised_flatfiles.zip
+
 wait
 # unzip files
 
@@ -100,19 +103,39 @@ tail -n +2 $ORIGINAL_FILE_PATH/"Measure Dates.csv" > $NEW_FILE_PATH/"Measure_Dat
 # Survey Result
 # filename: 
 
-tail -n +2 $ORIGINAL_FILE_PATH/"HCAHPS - Hospital.csv" > $NEW_FILE_PATH/"HCAHPS_Hospital.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_Efficiency_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_Efficiency_02_18_2016.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_hai_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_hai_02_18_2016.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_hcahps_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_hcahps_02_18_2016.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_imm2_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_imm2_02_18_2016.csv"
-#tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_outcome_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_outcome_02_18_2016.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_pn_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_pn_02_18_2016.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_scip_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_scip_02_18_2016.csv"
-tail -n +2 $ORIGINAL_FILE_PATH/"hvbp_tps_02_18_2016.csv" > $NEW_FILE_PATH/"hvbp_tps_02_18_2016.csv"
+  # inflating: hvbp_ami_05_28_2015.csv  
+  # inflating: hvbp_Efficiency_05_20_2015.csv  
+  # inflating: hvbp_hai_05_28_2015.csv  
+  # inflating: hvbp_hcahps_05_28_2015.csv  
+  # inflating: hvbp_hf_05_28_2015.csv  
+  # inflating: hvbp_outcome_05_18_2015.csv  
+  # inflating: hvbp_pn_05_28_2015.csv  
+  # inflating: hvbp_quarters.csv       
+  # inflating: hvbp_scip_05_28_2015.csv  
+  # inflating: hvbp_tps_05_28_2015.csv  
 
-#tail -n +2 $ORIGINAL_FILE_PATH/"Complications - Hospital.csv" > $NEW_FILE_PATH/"Complications - Hospital.csv"
+HVBP_AMI =  hvbp_ami_05_28_2015.csv 
+HVBP_EFFICIENCY =  hvbp_Efficiency_05_20_2015.csv
+HVBP_HAI = hvbp_hai_05_28_2015.csv
+HVBP_HCAHPS=hvbp_hcahps_05_28_2015.csv
+HVBP_HF=hvbp_hf_05_28_2015.csv
+HVBP_OUTCOME=hvbp_outcome_05_18_2015.csv
+HVBP_PN=hvbp_pn_05_28_2015.csv
+HVBP_QUARTERS=hvbp_quarters.csv
+HVBP_SCIP=hvbp_scip_05_28_2015.csv
+HVBP_TPS = hvbp_tps_05_28_2015.csv  
 
-#tail -n +2 $ORIGINAL_FILE_PATH/"Healthcare Associated Infections - Hospital.csv" > $NEW_FILE_PATH/"Healthcare Associated Infections - Hospital.csv"
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_AMI > $NEW_FILE_PATH/HVBP_AMI
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_EFFICIENCY > $NEW_FILE_PATH/HVBP_EFFICIENCY
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_HAI > $NEW_FILE_PATH/HVBP_HAI
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_HCAHPS> $NEW_FILE_PATH/HVBP_HCAHPS
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_HF > $NEW_FILE_PATH/HVBP_HF
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_OUTCOME > $NEW_FILE_PATH/HVBP_OUTCOME
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_PN > $NEW_FILE_PATH/HVBP_PN
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_QUARTERS > $NEW_FILE_PATH/HVBP_QUARTERS
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_SCIP > $NEW_FILE_PATH/HVBP_SCIP
+tail -n +2 $ORIGINAL_FILE_PATH/HVBP_TPS > $NEW_FILE_PATH/HVBP_TPS
+
 
 ###############################################
 # Structural Measures
@@ -169,8 +192,16 @@ hdfs dfs -put $NEW_FILE_PATH/$FILE_NAME $HDFS_EX1_PATH/$FILE_DIR
 hdfs dfs -ls $HDFS_EX1_PATH/$FILE_DIR
 
 
-FILE_NAME=hvbp_hcahps_02_18_2016.csv
-FILE_DIR=hvbp_hcahps_02_18_2016
+FILE_NAME=HVBP_HCAHPS
+FILE_DIR=hvbp_hcahps
+hdfs dfs -mkdir $HDFS_EX1_PATH/$FILE_DIR
+hdfs dfs -put $NEW_FILE_PATH/$FILE_NAME $HDFS_EX1_PATH/$FILE_DIR
+hdfs dfs -ls $HDFS_EX1_PATH/$FILE_DIR
+
+HVBP_OUTCOME=hvbp_outcome_05_18_2015.csv
+
+FILE_NAME=HVBP_OUTCOME
+FILE_DIR=hvbp_outcome
 hdfs dfs -mkdir $HDFS_EX1_PATH/$FILE_DIR
 hdfs dfs -put $NEW_FILE_PATH/$FILE_NAME $HDFS_EX1_PATH/$FILE_DIR
 hdfs dfs -ls $HDFS_EX1_PATH/$FILE_DIR
