cabfa110dcce7690f6edd8428b1c3aa785cc9ab9
James Peng <james.y.peng@gmail.com>
2016-07-02 19:21:46 +0800

    rename he spout and bolts

--- lab_6/wordcount.clj
+++ lab_6/wordcount.clj
@@ -5,40 +5,43 @@
 (defn wordcount [options]
    [
     ;; spout configuration
-    {"X-spout" (python-spout-spec
+    {"sentence-spout" (python-spout-spec
                 ;; topology options passed in
                 options
                 ;; name of the python class to "run"
                 "spouts.sentences.Sentences"
                 ;; output specification, what named fields will this spout emit
                 ["sentence"]
+				;;  two work threads
+				:p 1
 				)
     }
     
     ;; bolt configuration
-    {"Y-bolt"   (python-bolt-spec 
+    {"Parse-1-bolt"   (python-bolt-spec 
                 ;; topology options passed in
                 options
                 ;; inputs, where does this bolt receives its tuples from ?
-                {"X-spout" :shuffle}
+                {"sentence-spout" :shuffle}
                 ;; class to run 
                 "bolts.parse.ParseTweet"
                 ;; output spec, what tuples does this bolt emit
                 ["word"]
                 ;; configuration paramters
-                :p 2
+                :p 1
 				)
-    }
-    
+    } 
+	
+	
     ;; bolt configuration
-    {"Z-bolt"   (python-bolt-spec
+    {"TweetCounter-bolt" (python-bolt-spec
                 options
-                {"Y-bolt" :shuffle}
+                {"Parse-1-bolt" :shuffle}
                 ;; class to run 
                 "bolts.tweetcounter.TweetCounter"
                 ;; output spec, what tuples does this bolt emit
                 ["word" "count"]
-                :p 2
+                :p 1
 				)
     }
     ]
