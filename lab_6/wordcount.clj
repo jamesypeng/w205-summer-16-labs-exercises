cb98340085d5f8c63f3d5ee39acc87ce43b1d85e
James Peng <james.y.peng@gmail.com>
2016-07-03 09:19:22 +0800

    Add two parse bolts

--- lab_6/wordcount.clj
+++ lab_6/wordcount.clj
@@ -14,7 +14,7 @@
                 ;; output specification, what named fields will this spout emit
                 ["sentence"]
                 ;;  two work threads
-                :p 1
+                :p 2
                 )
     }
 	
@@ -31,12 +31,27 @@
                 ;; configuration paramters
                 :p 1
                 )
+		"Parse-2-bolt"   (python-bolt-spec 
+                ;; topology options passed in
+                options
+                ;; inputs, where does this bolt receives its tuples from ?
+                {"sentence-spout" :shuffle}
+                ;; class to run 
+                "bolts.parse.ParseTweet"
+                ;; output spec, what tuples does this bolt emit
+                ["word"]
+                ;; configuration paramters
+                :p 1
+                )
 
 		;; bolt configuration
 		"TweetCounter-bolt" (python-bolt-spec
                 options
                 ;; inputs,
-                {"Parse-1-bolt" ["word"]}
+                {
+				"Parse-1-bolt" ["word"]
+				"Parse-2-bolt" ["word"]
+				}
                 ;; class to run 
                 "bolts.tweetcounter.TweetCounter"
                 ;; output spec, what tuples does this bolt emit
