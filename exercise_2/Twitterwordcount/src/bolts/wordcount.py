63a50cbf057f13e57b3a6e8d15911e44e6256ce3
James Peng <james.y.peng@gmail.com>
2016-07-24 12:24:30 +0800

    Add psycopg2

--- exercise_2/Twitterwordcount/src/bolts/wordcount.py
+++ exercise_2/Twitterwordcount/src/bolts/wordcount.py
@@ -2,13 +2,14 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
+import psycopg2
 
-
-
+# psql --host=localhost --username=w205 --password --dbname=tcount 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
+		self.conn = psycopg2.connect(database="tcount", user="w205", password="postgres", host="localhost", port="5432")
 #        self.redis = StrictRedis()
 
     def process(self, tup):
@@ -19,7 +20,12 @@ class WordCounter(Bolt):
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
-        
+
+		cur = conn.cursor()
+		#cur.execute("SELECT upsert('k', 3 );)");
+		cur.execute("SELECT upsert(%s,%d)", (uWord, 1))
+        conn.commit()
+
 
         # Increment the local count
         self.counts[word] += 1
